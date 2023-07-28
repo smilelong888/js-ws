3fbcf9473385e08306bb0e96016d47fc0719d9bf
smilelong888 <126144142+smilelong888@users.noreply.github.com>
2023-07-28 22:04:34 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'cec5d260-b0a0-4674-a4f7-7360efbc84bb';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
